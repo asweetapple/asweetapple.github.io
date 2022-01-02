5d918e15bde2529df22a1ac20eadd8b1899aea92
asweetapple <sweetapplein1996@gmail.com>
2022-01-03 00:46:08 +0800

    maple_story_test

---
+++ js/maple_story.js
@@ -0,0 +1,81 @@
+window.onkeydown = function(e) {
+    return !(e.keyCode == 32);
+};
+
+/*
+  Handles a click on the down button to slide down the playlist.
+*/
+document.getElementsByClassName('down-header')[0].addEventListener('click', function() {
+    var list = document.getElementById('list');
+
+    list.style.height = (parseInt(document.getElementById('flat-black-player-container').offsetHeight) - 135) + 'px';
+
+    document.getElementById('list-screen').classList.remove('slide-out-top');
+    document.getElementById('list-screen').classList.add('slide-in-top');
+    document.getElementById('list-screen').style.display = 'block';
+});
+
+/*
+  Handles a click on the up arrow to hide the list screen.
+*/
+document.getElementsByClassName('hide-playlist')[0].addEventListener('click', function() {
+    document.getElementById('list-screen').classList.remove('slide-in-top');
+    document.getElementById('list-screen').classList.add('slide-out-top');
+    document.getElementById('list-screen').style.display = 'none';
+});
+
+/*
+  Handles a click on the song played progress bar.
+*/
+document.getElementById('song-played-progress').addEventListener('click', function(e) {
+    var offset = this.getBoundingClientRect();
+    var x = e.pageX - offset.left;
+
+    Amplitude.setSongPlayedPercentage(parseFloat(x) / parseFloat(this.offsetWidth) * 100);
+});
+
+document.querySelector('img[data-amplitude-song-info="cover_art_url"]').style.height = document.querySelector('img[data-amplitude-song-info="cover_art_url"]').offsetWidth + 'px';
+
+var songs = [
+    {
+        'name': '天空之城主城區',
+        'artist': 'maple_story',
+        'album': 'n/a',
+        'url': 'music/maple_story/天空之城主城區.mp3',
+        'cover_art_url': 'https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-to-answer.jpg'
+    },
+    {
+        'name': '天空之船 - 正常航行中',
+        'artist': 'maple_story',
+        'album': 'n/a',
+        'url': 'music/maple_story/天空之船 - 正常航行中.mp3',
+        'cover_art_url': 'https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-to-answer.jpg'
+    }
+];
+
+for (index = 0; index < songs.length; index++) {
+    var listItem = `<div class="song amplitude-song-container amplitude-play-pause" data-amplitude-song-index="${index}">
+                        <span class="song-number-now-playing">
+                            <span class="number">${index + 1}</span>
+                            <img class="now-playing" src="https://521dimensions.com/img/open-source/amplitudejs/examples/flat-black/now-playing.svg" />
+                        </span>
+                        <div class="song-meta-container">
+                            <span class="song-name" data-amplitude-song-info="name" data-amplitude-song-index="${index}"></span>
+                            <span class="song-artist-album">
+                                <span data-amplitude-song-info="artist" data-amplitude-song-index="${index}"></span>
+                                -
+                                <span data-amplitude-song-info="album" data-amplitude-song-index="${index}"></span>
+                            </span>
+                        </div>
+                    </div>`;
+    $("#list").append(listItem);
+}
+
+Amplitude.init({
+    "bindings": {
+        37: 'prev',
+        39: 'next',
+        32: 'play_pause'
+    },
+    "songs": songs
+});
